6a5b0aaa280169fef5b291477b4f49ee3f6ee777
Huzaifa Quadri <huzaifaquadri1853@gmail.com>
2026-03-28 11:25:05 +0530

    Setting up frotnend directory structure + tailwind + redux

---
+++ frontend/src/app/App.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { RouterProvider } from "react-router-dom";
+import { route } from "./app.routes";
+
+const App = () => {
+  return <RouterProvider router={route} />;
+};
+
+export default App;
