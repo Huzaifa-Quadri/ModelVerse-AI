145791f8d88f868d9d0f62559a1164b2fd052c13
Huzaifa Quadri <huzaifaquadri1853@gmail.com>
2026-04-05 13:13:27 +0530

    missing function

--- backend/src/services/ai.service.js
+++ backend/src/services/ai.service.js
@@ -25,3 +25,24 @@ export async function generateResponse(message) {
     throw error;
   }
 }
+
+export async function generateTitle(message) {
+  try {
+    if (!process.env.MISTRAL_API_KEY) {
+      throw new Error("MISTRAL_API_KEY is not set in environment");
+    }
+
+    const response = await mistralModel.invoke([
+      new SystemMessage(`You are a helpful assistant that generates concise and catchy titles for the chat conversations. 
+        User will provide you with first message of the conversation and you will generate a title based on that. The title should be relevant to the content of the conversation and should be engaging to attract users. Please keep the title short and to the point, giving users a clear idea of what the conversation is about.
+      `),
+      new HumanMessage(
+        `Please provide a title that captures the essence of the given message: "${message}"`,
+      ),
+    ]);
+    return response.content || response.text;
+  } catch (error) {
+    console.error("❌ AI Generation Error:", error.message);
+    throw error;
+  }
+}
